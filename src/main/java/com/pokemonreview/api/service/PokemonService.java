fac4265e22b9c4a4f03b0217a81ba44622861a27
Punkrig <igorbrandaao@gmail.com>
2025-04-14 17:28:07 -0300

    Desenvolvimento: Detail & Update criados

--- src/main/java/com/pokemonreview/api/service/PokemonService.java
+++ src/main/java/com/pokemonreview/api/service/PokemonService.java
@@ -8,4 +8,8 @@ public interface PokemonService {
     PokemonDto createPokemon(PokemonDto pokemonDto);
 
     List<PokemonDto> getAllPokemon();
+
+    PokemonDto getPokemonById(int id);
+
+    PokemonDto updatePokemon(PokemonDto pokemonDto, int id);
 }
